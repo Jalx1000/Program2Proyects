2459d93d5cede90ec912c83497cdc5dda894dfb8
Javier Mogro <jalejandromogro1@gmail.com>
2023-07-10 22:18:36 -0400

    Program 2

---
+++ 1/UArchivosTexto.cpp
@@ -0,0 +1,138 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "UArchivosTexto.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+#include <fstream>
+#include <iostream>
+TForm1* Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner) : TForm(Owner) {}
+int height = 60;
+int widht = 60;
+//---------------------------------------------------------------------------
+void TForm1::Cargar(String nom)
+{
+    byte i, j;
+    char img;
+    ifstream pf(nom.c_str());
+    if (!pf.fail()) { //si logró abrir el archivo
+        i = 0;
+        j = 0;
+        while (!pf.eof()) {
+            img = pf.get();
+            Laberinto[j][i] = img;
+            switch (img) {
+                case '#':
+					ImageList1->Draw(Canvas, j * widht, i * height, 0);
+					break;
+                case 'h':
+					ImageList1->Draw(Canvas, j * widht, i * height, 1);
+                    x = j;
+                    y = i;
+                    break;
+				case 'p':
+					ImageList1->Draw(Canvas, j * widht, i * height, 2);
+                    break;
+				case ' ':
+					ImageList1->Draw(Canvas, j * widht, i * height, 3);
+					break;
+                case 10:
+                    n = j;
+                    i++;
+                    j = 255;
+                    break;
+            }
+            j++;
+        }
+        m = i;
+		Height = (m + 2) * height + 22;
+		Width = n * widht + 17;
+        pf.close();
+    }
+}
+
+bool TForm1::BackTrack(char Lab[][100], int x, int y)
+{
+    bool haySalida = false;
+    byte nc;
+    char d;
+    char caminos[4];
+    nc = 0;
+    if ((Lab[x + 1][y] == ' ') ||
+        (Lab[x + 1][y] == 'p')) //si hay camino a la derecha
+        caminos[nc++] = '6';
+    if ((Lab[x - 1][y] == ' ') ||
+        (Lab[x - 1][y] == 'p')) //si hay camino a la izquierda
+        caminos[nc++] = '4';
+    if ((Lab[x][y - 1] == ' ') || (Lab[x][y - 1] == 'p')) //si hay camino arriba
+        caminos[nc++] = '8';
+    if ((Lab[x][y + 1] == ' ') || (Lab[x][y + 1] == 'p')) //si hay camino abajo
+        caminos[nc++] = '2';
+    while ((nc > 0) && (!haySalida)) {
+        nc--;
+        d = caminos[nc];
+        Lab[x][y] = '+'; //camino pisado
+		ImageList1->Draw(Canvas, x * widht, y * height, 3); //borra al hamster
+        switch (d) {
+            case '4':
+                x--;
+                break;
+            case '6':
+                x++;
+                break;
+            case '8':
+                y--;
+                break;
+            case '2':
+                y++;
+                break;
+        }
+		ImageList1->Draw(Canvas, x * widht, y * height, 1); //dibuja al hamster
+        Sleep(100);
+        if (Lab[x][y] == 'p')
+            haySalida = TRUE;
+        else {
+            haySalida = BackTrack(Lab, x, y);
+            if (!haySalida) { //back track
+				ImageList1->Draw(Canvas, x * widht, y * height, 3); //borra al hamster
+                switch (d) {
+                    case '4':
+                        x++;
+                        break;
+                    case '6':
+                        x--;
+                        break;
+                    case '8':
+                        y++;
+                        break;
+                    case '2':
+                        y--;
+                        break;
+                }
+				ImageList1->Draw(Canvas, x * widht, y * height, 1); //dibuja al hamster
+                Sleep(100);
+            }
+        }
+    }
+    return haySalida;
+}
+
+void __fastcall TForm1::Cargar1Click(TObject* Sender)
+{
+    if (OpenTextFileDialog1->Execute()) {
+        Cargar(OpenTextFileDialog1->FileName);
+    }
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Probar1Click(TObject* Sender)
+{
+    BackTrack(Laberinto, x, y);
+}
+//---------------------------------------------------------------------------
+
---
+++ 1/UArchivosTexto.h
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------
+
+#ifndef UArchivosTextoH
+#define UArchivosTextoH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <System.ImageList.hpp>
+#include <Vcl.Dialogs.hpp>
+#include <Vcl.ExtDlgs.hpp>
+#include <Vcl.ImgList.hpp>
+#include <Vcl.Menus.hpp>
+#include <Vcl.BaseImageCollection.hpp>
+#include <Vcl.ImageCollection.hpp>
+
+#include <fstream>
+#include <iostream>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+  __published: // IDE-managed Components
+    TMainMenu* MainMenu1;
+	TImageList* ImageList1;
+    TOpenTextFileDialog* OpenTextFileDialog1;
+	TMenuItem* Probar1;
+	TMenuItem* Cargar1;
+	TImageCollection *ImageCollection1;
+	void __fastcall Cargar1Click(TObject *Sender);
+	void __fastcall Probar1Click(TObject *Sender);
+  private: // User declarations
+    char Laberinto[100][100];
+    byte m, n; // para la dimensión de la matriz del mapa de bits
+	int x, y; //las coordenadas del hámster
+  public: // User declarations
+    __fastcall TForm1(TComponent* Owner);
+	void Cargar(String nom); //carga el mapa de bits a partir del archivo txt
+	bool BackTrack(char Lab[][100], int x, int y); //recorre el laberinto buscando la salida. TRUE si lo encuentra.
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1* Form1;
+//---------------------------------------------------------------------------
+#endif
+
